d033233da565bd66a7319f1c8f4db3a22db6bbfa
Sara Martins <73107712+comandoPipas@users.noreply.github.com>
2022-12-09 18:12:39 +0000

    just having an itty bitty fun

--- m12816-e10973.py
+++ m12816-e10973.py
@@ -1,7 +1,21 @@
 """
-Aprendizagem Automática -- Projeto Final
+Aprendizagem Automatica -- Projeto Final
 
 M12816 - Cristiano Santos
 E10973 - Sara Martins
 """
 
+""" BIBLIOTECAS """
+
+import numpy as np
+import pandas as pd
+
+""" VARIAVEIS """
+
+""" CODIGO """
+
+# abrir o csv com o dataset
+email = pd.read_csv('spam_ham_dataset.csv')
+
+# mostrar as primeiras cinco entradas do dataset
+print (email.head())
